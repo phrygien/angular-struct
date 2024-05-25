1669161d2c0933f30d991f26d3575494fcd6bc06
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-05-25 16:04:54 +0300

    mise en place routage module admin

--- src/app/admin/cocktail/cocktail-routing.module.ts
+++ src/app/admin/cocktail/cocktail-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CIndexComponent } from './c-index/c-index.component';
+import { CAddComponent } from './c-add/c-add.component';
+import { CEditComponent } from './c-edit/c-edit.component';
+import { CDeleteComponent } from './c-delete/c-delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CIndexComponent },
+  { path: 'add', component: CAddComponent },
+  { path: 'edit/:id', component: CEditComponent },
+  { path: 'delete/:id', component: CDeleteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './_utils/error/error.component';
 
 const routes: Routes = [
   {
     // charge le module et active le apres (Lazy load)
     path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule),
-  }
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then( a => a.AdminModule),
+  },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
