a78b9d5eb962209adc4591ceb480d9aab455ab5d
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-05-25 17:40:42 +0300

    recuperation params url

--- src/app/admin/user/u-delete/u-delete.component.ts
+++ src/app/admin/user/u-delete/u-delete.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-u-delete',
   templateUrl: './u-delete.component.html',
   styleUrl: './u-delete.component.css'
 })
-export class UDeleteComponent {
+export class UDeleteComponent implements OnInit {
 
+  constructor(private activated: ActivatedRoute){}
+
+  ngOnInit(): void {
+      this.activated.params.subscribe(
+        (data) => {
+          console.log(data);
+        }
+      )
+  }
 }
--- src/app/admin/user/u-edit/u-edit.component.ts
+++ src/app/admin/user/u-edit/u-edit.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-u-edit',
   templateUrl: './u-edit.component.html',
   styleUrl: './u-edit.component.css'
 })
-export class UEditComponent {
+export class UEditComponent implements OnInit {
+  constructor(private activated: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.activated.params.subscribe(
+      (data) => {
+        console.log(data)
+      }
+    )
+  }
 }
