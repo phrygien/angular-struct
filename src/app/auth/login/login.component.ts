8d7e14435fce54865c2bfeb720cc7effc24af226
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-05-26 14:24:42 +0300

    formulaire authentification

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  //creation objet form
+  form= {
+    email: null,
+    password: null,
+  }
+  constructor(){}
 
+  ngOnInit(): void {
+      initFlowbite();
+  }
 }
